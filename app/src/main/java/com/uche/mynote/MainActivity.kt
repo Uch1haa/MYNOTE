f40748a426dd86605973a0e0e825215031a9829e
willxy21 <williamsuche77@gmail.com>
2021-07-06 14:49:14 +0100

    note update

--- app/src/main/java/com/uche/mynote/MainActivity.kt
+++ app/src/main/java/com/uche/mynote/MainActivity.kt
@@ -2,14 +2,17 @@ package com.uche.mynote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
 import com.uche.mynote.databinding.ActivityMainBinding
 import com.uche.mynote.models.Note
+import com.uche.mynote.models.NoteAdapter
 import com.uche.mynote.models.NoteDatabase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var database: NoteDatabase
+    private lateinit var notesAdapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +23,23 @@ class MainActivity : AppCompatActivity() {
             applicationContext,
             NoteDatabase::class.java,
             "notes_database"
-        ).build()
+        ).allowMainThreadQueries().build()
+
+        notesAdapter = NoteAdapter(database.noteDao().getAllNotes())
+        binding.Recycler.apply{
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = notesAdapter
+        }
+        binding.button.setOnClickListener {
+            val tittle = binding.tittle.text.toString()
+            val content = binding.content.text.toString()
+
+            saveNote(tittle, content)
+        }
     }
     private fun saveNote(tittle: String, content: String){
         val note = Note(id = 0, tittle, content)
+        database.noteDao().addNote(note)
+        notesAdapter.notifyDataSetChanged()
     }
 }
